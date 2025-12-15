65d0de4f16cdf35283b9e2ae4df02b020599d553
yue.tan <17526331783@qq.com>
2025-12-15 06:42:11 +0000

    trades测试数据载入

--- test/okx/dataset.py
+++ test/okx/dataset.py
@@ -1,6 +1,7 @@
-from hft_backtest import CsvDataset
+from hft_backtest import CsvDataset, ParquetDataset
 from itertools import product
 from datetime import datetime
+from hft_backtest.okx.event import OKXTrades, OKXBookticker
 
 renmae_dict = {
     f"{bidask}[{num - 1}].{priceamount}": f"{bidask}_{priceamount}_{num}"
@@ -11,10 +12,26 @@ ds = CsvDataset(
     name="book_ticker",
     path="/shared_dir/Tan/okex_L2_0801/BTC-USDT-SWAP/okex-swap_book_snapshot_25_2025-08-01_BTC-USDT-SWAP.csv.gz",
     timecol="timestamp",
+    event_type=OKXBookticker,
     compression="gzip",
     rename=renmae_dict,
 )
 
+dt1 = datetime.now()
+for data in ds:
+    # print(data)
+    # break
+    pass
+dt2 = datetime.now()
+print("Elapsed time:", dt2 - dt1)
+
+ds2 = ParquetDataset(
+    name="trades",
+    path="trades_2025-08-01.parquet",
+    timecol="created_time",
+    event_type=OKXTrades,
+    rename={"instrument_name":'symbol'},
+)
 dt1 = datetime.now()
 for data in ds:
     # print(data)

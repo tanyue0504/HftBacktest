9543785c7e0f759dfc0c39062f0a81c3419a1c87
yue.tan <17526331783@qq.com>
2025-12-15 02:00:50 +0000

    重新定义recorder基类行为

--- hft_backtest/recorder.py
+++ hft_backtest/recorder.py
@@ -1,22 +1,22 @@
-from hft_backtest import Component, Order, Data,  EventEngine
 from abc import abstractmethod, ABC
+from hft_backtest import Component, EventEngine, Order
 
 class Recorder(Component, ABC):
-    def __init__(self):
+    """
+    记录器基类
+    负责记录交易流水(Trades)和定期快照(Snapshots)
+    """
+    def __init__(self, dir_path: str):
+        self.dir_path = dir_path
+
+    @abstractmethod
+    def record_trade(self, order: Order):
+        """记录成交明细"""
         pass
 
     @abstractmethod
-    def on_order(self, order: Order):
+    def record_snapshot(self):
+        """执行一次快照记录"""
         pass
-
-    @abstractmethod
-    def on_data(self, data: Data):
-        pass
-
-    def start(self, event_engine: EventEngine):
-        self.event_engine = event_engine
-        event_engine.register(Order, self.on_order)
-        event_engine.register(Data, self.on_data)
-
-    def stop(self):
-        pass
+    
+    # 子类需要实现 start/stop 方法来注册监听
